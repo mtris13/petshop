c21ecdbbc585d6ae81114f255edb32508babf100
Minh Tri <tri13t4@gmail.com>
2025-09-10 23:24:06 +0700

    Add controllers

---
+++ include/domain/entities/Account.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Account {
+private:
+  string AccountID;
+  string AccountName;
+  string Password;
+  string Gender;
+
+public:
+};
